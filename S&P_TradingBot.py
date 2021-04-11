b510c73dab3ece86a827704de89094327436995b
Qubit <jiamu.danielwang@gmai.com>
2021-04-11 17:12:13 +1000

    S&P 500 Equal Weighted Python Bot Finished

--- S&P_TradingBot.py
+++ S&P_TradingBot.py
@@ -3,3 +3,142 @@ import pandas as pd
 import requests
 import xlsxwriter
 import math 
+from secrets import IEX_CLOUD_API_TOKEN
+
+# Importing the list of stocks
+stocks = pd.read_csv('sp_500_stocks.csv')
+symbol = 'AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote/?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+
+# Parsing the API call
+price = data['latestPrice']
+market_cap = data['marketCap']
+
+# Adding stocks data to a Pandas DataFrame
+my_columns = [ 'Ticker', 'Stock Price', 'Market Capitalization', 'Number of Shares to Buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+final_dataframe.append(
+    pd.Series(
+    [
+        symbol,
+        price,
+        market_cap,
+        'N/A'
+    ],
+    index = my_columns
+    ),
+    ignore_index=True
+)
+
+# Looping through Tickers in list of stocks
+final_dataframe = pd.DataFrame(columns = my_columns)
+for stock in stocks['Ticker'][:5]:
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{stock}/quote/?token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(api_url).json()
+    final_dataframe = final_dataframe.append(
+        pd.Series(
+        [
+            stock,
+            data['latestPrice'],
+            data['marketCap'],
+            'N/A'
+        ],
+        index = my_columns
+        ),
+        ignore_index=True
+    )
+
+# Batch API calls to improve performance
+def chunks(lst, n):
+    """Yield successive n-sized chinks from list."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for symbol_string in symbol_strings:
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
+            pd.Series(
+            [
+                symbol,
+                data[symbol]['quote']['latestPrice'],
+                data[symbol]['quote']['marketCap'],
+                'N/A'
+            ],
+            index = my_columns
+            ),
+            ignore_index=True
+        )
+
+
+# Calculating number of shares to buy
+while True:
+    try:
+        portfolio_size = input("Please enter the value of your portfolio: ")
+        portfolio_size = int(portfolio_size)
+        break
+    except ValueError:
+        print("*** Not a valid integer entered! Please try again ... ***")
+print("*** Great! Calculating number of shares to buy now ***")
+
+val = float(portfolio_size)
+position_size = val/len(final_dataframe.index)
+
+for i in range(0, len(final_dataframe.index)):
+    final_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size/final_dataframe.loc[i, 'Stock Price'])
+
+# Formatting Excel output
+writer = pd.ExcelWriter('recommended trades.xlsx', engine = 'xlsxwriter')
+final_dataframe.to_excel(writer,'Recommended Trades', index = False)
+
+background_color = '#2c2f33'
+font_color = '#ffffff'
+
+string_format = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_format = writer.book.add_format(
+        {
+            'num_format':'$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_format = writer.book.add_format(
+        {
+            'num_format':'0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+# Applying the format to the coloumns of the .xlsx file
+column_formats = {
+    'A': ['Ticker', string_format],
+    'B': ['Stock Price', dollar_format],
+    'C': ['Market Capitalization', dollar_format],
+    'D': ['Number of Shares to Buy', integer_format]
+}
+
+for column in column_formats.keys():
+    writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 18, column_formats[column][1])
+    writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+writer.save()
